9bd21a5599d167f81c89e716b3e527fd1b3c89d2
Gabriel Chin <gabchinn2759@gmail.com>
2023-01-06 10:13:03 +1100

    added grid of buttons and selection radio buttons

--- game.py
+++ game.py
@@ -1,29 +1,57 @@
-# Simple pygame program
+# Importing libraries 
+import tkinter as tk
 
-# Import and initialize the pygame library
-import pygame
-pygame.init()
+# Initializing window
+root = tk.Tk()
+# root.resizable(height=False, width=False)
+root.title('Maze Path Finder')
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+# Initializing play area
+play_area = tk.Frame(root, width=800, height=500, bg="white")
+play_area.grid(row=0, column=0, padx=10, pady=10)
 
-# Run until the user asks to quit
-running = True
-while running:
+# Invisible 1x1 pixel to make button square
+btn_img = tk.PhotoImage("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=")
 
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+# Defining block class for configuring 
+class Block:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+        self.value = None # value be start, stop, block or none
+        self.button = tk.Button(play_area, image = btn_img, text='', bg='white', width = 50, height=50)
+        self.button.grid(row=x, column=y)
+    
+    def reset(self):
+        self.button.configure(text='', bg='white')
+        self.value = None
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+# Generating a grid in the play area
+for x in range(1, 11):
+    for y in range(1, 11):
+        Block(x, y)
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
 
-    # Flip the display
-    pygame.display.flip()
+# Initializing selection area
+selection_area = tk.Frame(root)
+selection_area.grid(row=0, column=1, padx=10, pady=10)
 
-# Done! Time to quit.
-pygame.quit()
+v = tk.IntVar()
+
+# Dictionary to create multiple buttons
+func_values = {
+    "start": 1,
+    "stop" : 2,
+    "brick" : 3,
+    "reset" : 4
+}
+
+for (text, value) in func_values.items():
+    tk.Radiobutton(selection_area, text=text, variable=v, value=value).pack()
+
+
+
+
+
+
+root.mainloop()
