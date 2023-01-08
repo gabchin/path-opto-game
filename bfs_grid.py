c08422009a8bf2b3a566962a77218c98437b1fb3
Gabriel Chin <gabchinn2759@gmail.com>
2023-01-08 19:04:15 +1100

    bfs_grid code cleaned up a bit

--- bfs_grid.py
+++ bfs_grid.py
@@ -1,86 +1,77 @@
 import numpy as np
 from collections import deque
 
-"""
-Block Types
-0: empty
-1: start
-2: end
-3: brick
-"""
-
 # For testing purposes
-block_map = np.array([[1, 3, 0, 0, 0], [0, 3, 0, 0, 0], [0, 0, 3, 0, 0], [0, 0, 0, 3, 0], [0, 0, 0, 0, 2]])
-
-
+block_map = np.array([[1, 3, 0, 0, 0], 
+                      [0, 0, 0, 0, 0], 
+                      [0, 0, 3, 0, 0], 
+                      [0, 0, 0, 3, 0], 
+                      [0, 0, 0, 0, 2]])
 
 # Assume sx, sy, ex, ey given (since can be taken from button pressed)
 x_start = 0
 y_start = 0
-x_end = 4
-y_end = 4
 
-# Algo start
-row = len(block_map)
-col = len(block_map[1])
+def bfs_shortest_path(x_start, y_start):
+    # Defining row for setting grid boundary
+    row = len(block_map)
+    col = len(block_map[1])
 
-visited = np.full((row, col), False)
-#prev_node = [[None]*row for i in range(col)]
-x_queue = deque([x_start])
-y_queue = deque([y_start])
-visited[y_start][x_start] = True
+    # Defining matrix for tracking explored node and parent node
+    explored = np.full((row, col), False)
+    parent = np.full((row, col), None)
 
-while x_queue:
-    # Popping from queue and indicate it has been visited
-    x = x_queue.popleft()
-    y = y_queue.popleft()
+    # Defining queues for x and y to keep track of nodes that
+    # need to be explored
+    x_queue = deque([x_start])
+    y_queue = deque([y_start])
+    explored[y_start][x_start] = True
+    
+    # Looping over if there is a node in the queues (meaning there are explored but unexplored node)
+    while x_queue:
+        # Popping from queue to get node that needs to be explored
+        x = x_queue.popleft()
+        y = y_queue.popleft()
 
-    print(visited)
-    print()
+        # Break when end point is found
+        if block_map[y][x] == 2:
+            break
+        
+        explore_neighbour(x, y, block_map, explored, row, col, x_queue, y_queue)
 
-    if block_map[y][x] == 2:
-        visited[y][x] = True
-        print("TA DA!")
-        break
+    return explored
 
+def explore_neighbour(x, y, map, explored, row, col,  x_queue, y_queue):
     # Exploring neighbour of current coordinate (x, y) by up, right, down, left
     dx = [0, 1, 0, -1]
     dy = [-1, 0, 1, 0]
-    
 
     for i in range(len((dx))):
-
         xx = x + dx[i]
         yy = y + dy[i]
 
-
         # Seeing if move is valid
-        # Out of bounds
-        if xx >= row or yy >= row: 
-            # print(f"out of bounds {xx}, {yy}")
+        if xx >= row or yy >= col: # Out of bounds condition.
             continue
-        if xx < 0 or yy < 0: 
-            # print(f"out of bounds {xx}, {yy}")
+        if xx < 0 or yy < 0: # Out of bounds condition.
             continue
-        # block is visted or a brick
-        if visited[yy][xx] == True:
-            # print(f"visited {xx}, {yy}")
+        if explored[yy][xx] == True: # block is visted.
             continue
-        if block_map[yy][xx] == 3:
-            # print(f"blocked {xx}, {yy}")
+        if map[yy][xx] == 3: # Block is a brick.
             continue
 
-        # If valid move, add to the queue
+        # If valid move, add to the queue for visiting.
         x_queue.append(xx)
         y_queue.append(yy)
 
-        visited[yy][xx] = True
-    
+        # Mark valid coordiate as explored.
+        explored[yy][xx] = True
+
+
+print(bfs_shortest_path(x_start, y_start))
 
         
-
     
-  
 
 
 
